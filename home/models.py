e5301e45926d98250fdc6ed976ccfd46ef071bdf
Aditya Dev Joshi <aditjoshi2005@gmail.com>
2026-03-03 13:14:04 +0545

    Learned Models and Migrations

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # id = models.AutoField()
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(default=18)
+    email = models.EmailField( null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    file = models.FileField()
+    
+
+class Product(models.Model):
+    pass
+
+
