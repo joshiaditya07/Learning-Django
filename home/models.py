8a46c3256e3f921ccda98ea814938e1894fe2c30
Aditya Dev Joshi <aditjoshi2005@gmail.com>
2026-03-04 14:02:51 +0545

    Learned about Django Shell

--- home/models.py
+++ home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-
+# model means table in database: basically we are creating a table in database with the help of models.py file. we can create as many tables as we want in database by creating models in models.py file. each model will represent a table in database. each attribute of model will represent a column
 class Student(models.Model):
     # id = models.AutoField()
     name = models.CharField(max_length=100)
---
+++ home/utils.py
@@ -0,0 +1,7 @@
+from .models import Student
+import time
+def run_this_function():
+    print("Function started")
+    time.sleep(1) # This will make the function wait for 1 second before executing the next line of code
+    print("Function Executed")
+
