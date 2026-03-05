6986b7a0cf7f58896941f6dc2cc2c715222a6029
Aditya Dev Joshi <aditjoshi2005@gmail.com>
2026-03-05 13:22:44 +0545

    Learned CRUD operations

--- home/models.py
+++ home/models.py
@@ -8,10 +8,16 @@ class Student(models.Model):
     age = models.IntegerField(default=18)
     email = models.EmailField( null=True, blank=True)
     address = models.TextField(null=True, blank=True)
-    file = models.FileField()
+
     
 
 class Product(models.Model):
     pass
 
 
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+
+    def __str__(self)-> str:
+        return self.car_name
